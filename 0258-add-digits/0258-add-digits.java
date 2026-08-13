3edee2220d881372e5ca199b697dc9f9d159c1b6
kushalgupta-max <2k24.csds1b.2410952@gmail.com>
2026-08-13 10:52:08 +0530

    Time: 1 ms (97.97%), Space: 42.7 MB (25.94%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,18 +1,6 @@
 class Solution {
     public int addDigits(int n) {
-        int r=0;
-        int pro=0;
-        while(n!=0)
-        {
-            r=n%10;
-            pro=pro+r;
-            n=n/10;
-            if(pro>9&&n==0)
-            {
-                n=pro;
-                pro=0;
-            }
-        }
-        return pro;
+        if (n == 0) return 0;
+        return 1 + (n - 1) % 9;
     }
 }
