8921de6a9bb3994a9c0903c0a11eca12749470a7
kushal gupta <192629059+kushalgupta-max@users.noreply.github.com>
2026-09-12 17:04:35 +0530

    Time: 2086 ms (52.97%), Space: 42.3 MB (25.58%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,12 +1,17 @@
 class Solution {
     public boolean checkPerfectNumber(int l) {
         int sum=0;
+        HashSet<Integer>set=new HashSet<>();
         for(int i=1;i<=l/2;i++)
         {
             if(l%i==0)
             {
-                sum=sum+i;
+                set.add(i);
             }
+        }
+        for(int freq:set)
+        {
+            sum=sum+freq;
         }
         if(sum!=l)
         {
